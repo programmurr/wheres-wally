bf4839d8ee83ed2f5fe1f724a9b881f6bf72f2d1
conor <conorjm89@gmail.com>
2021-06-28 22:47:20 +0100

    Timer now working with leading zeros

--- src/components/nav-bar/Timer.js
+++ src/components/nav-bar/Timer.js
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
 function Timer() {
-  const [ time, setTime ] = useState(0)
+  const [ secondsLabel, setSecondsLabel ] = useState('00');
+  const [ minutesLabel, setMinutesLabel ] = useState('00');
+  const [ totalSeconds, setTotalSeconds ] = useState(0);
+
+  const pad = (time) => {
+    let timeString = time + "";
+    if (timeString.length < 2) {
+      return '0' + timeString;
+    } else {
+      return timeString;
+    }
+  }
 
   useEffect(() => {
     let interval = setInterval(() => {
-      setTime(time + 1);
+      const newTotal = totalSeconds + 1;
+      const newSecondsLabel = pad(newTotal % 60);
+      const newMinutesLabel = pad(parseInt(newTotal / 60));
+      setSecondsLabel(newSecondsLabel);
+      setMinutesLabel(newMinutesLabel);
+      setTotalSeconds(parseInt(newTotal));
     }, 1000);
     return () => clearInterval(interval);
   });
 
   return (
     <div>
-      {time} seconds
+      <p>{minutesLabel}:{secondsLabel}</p>
     </div>
   )
 }
